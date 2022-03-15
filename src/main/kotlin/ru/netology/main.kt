fb4e47cf8b86a8772ac74f3251c43800f80d6640
Alexey Potapov <aloy@bk.ru>
2022-03-15 15:17:58 +0300

    main fun created

---
+++ src/main/kotlin/ru/netology/main.kt
@@ -0,0 +1,41 @@
+package ru.netology
+
+fun main() {
+    println("Пользователь был в сети " + agoToText(45))
+    for (i in 1..8) {
+        println("Пользователь был в сети " + agoToText(60 * i * i + 5))
+    }
+    for (i in 1..5) {
+        println("Пользователь был в сети " + agoToText(60 * 60 * (i * i + 2) + 5))
+    }
+
+}
+
+fun agoToText(pastTime: Int): String {
+    return when {
+        pastTime <= 60 -> "только что"
+        pastTime <= 3600 -> minutesAgo(pastTime)
+        pastTime <= 24 * 60 * 60 -> hoursAgo(pastTime)
+        else -> "давно"
+    }
+}
+
+fun minutesAgo(pastTime: Int): String {
+    val minutes = pastTime / 60
+    return when {
+        minutes in 5..20 -> "$minutes минут назад"
+        minutes % 10 == 1 -> "$minutes минуту назад"
+        minutes % 10 in 2..4 -> "$minutes минуты назад"
+        else -> "$minutes минут назад"
+    }
+}
+
+fun hoursAgo(pastTime: Int): String {
+    val hours = pastTime / 3600
+    return when {
+        hours in 5..20 -> "$hours часов назад"
+        hours % 10 == 1 -> "$hours час назад"
+        hours % 10 in 2..4 -> "$hours часа назад"
+        else -> "$hours часов назад"
+    }
+}
